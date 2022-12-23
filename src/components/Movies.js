51889c0666fd58f48975729c842b202f61bb7b6f
Matthew <mattj20@gmail.com>
2022-12-23 12:11:50 -0800

    commit tree 3b6263ce7c6526578f8ebefb823a80982b8c1ac8

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,17 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => 
+          <div>
+            <h4>Name: {movie.title}</h4>
+            <p>Time: {movie.time}</p>
+            <p>Genres:</p>
+            <ul>
+              {movie.genres.map(genre => <li>{genre}</li>)}
+            </ul>
+          </div>
+          )}
     </div>
   );
 };
